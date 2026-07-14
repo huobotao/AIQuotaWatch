886acf00b543d9bc1b4d9fdfc8715f1c4226f8ec
huobotao <huobotao@hotmail.com>
2026-07-14 18:21:26 +1000

    Add work/AIQuotaPhone/AIQuotaPhone

---
+++ work/AIQuotaPhone/AIQuotaPhone/AIQuotaPhoneApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct AIQuotaPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
